b4c52ec3346ae37b2f952b61dd015fc078b2952b
Kalashkov Paul <kalashkovpaul@yandex.ru>
2022-04-10 13:29:58 +0300

    Lab05 is done?

---
+++ lab05/mainwindow.h
@@ -0,0 +1,62 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QGraphicsPixmapItem>
+#include <QGraphicsScene>
+#include <QMainWindow>
+#include <QMouseEvent>
+#include <QVector>
+#include <QPixmap>
+#include <QImage>
+
+namespace Ui {
+class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+	Q_OBJECT
+
+public:
+	explicit MainWindow(QWidget *parent = 0);
+	~MainWindow();
+
+private slots:
+	void mousePressEvent(QMouseEvent *event);
+	void on_addPointPushButton_clicked();
+	void on_closePushButton_clicked();
+	void on_fillPushButton_clicked();
+	void on_clearPushButton_clicked();
+	void on_setColorPushButton_clicked();
+
+private:
+	Ui::MainWindow *ui;
+
+	bool closed;
+	QVector<QPoint> points;
+	QVector<QLine> edges;
+	QVector<QPoint> intersections;
+    QPoint firstPoint;
+
+	QPixmap pixmap;
+	QImage image;
+	const QColor defaultBoundColor = Qt::black;
+	const QColor defaultFillColor = QColor(2, 2, 2);
+	QColor fillColor;
+    QColor fillColorInversion;
+
+	int n_edges;
+    int max_x;
+	enum DrawType {	none, horizontal, vertical, diagonal };
+	void addPoint(const QPoint &point, DrawType drawType);
+	void addEdge(const QLine &edge);
+	void dda(const QLine &edge);
+    void fixVertices(QVector<QPoint>& intersections);
+
+	void delay(int);
+	void clearImage();
+	void displayImage();
+	void colorLabel();
+};
+
+#endif // MAINWINDOW_H
